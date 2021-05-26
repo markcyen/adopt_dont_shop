96f4f299ee3522964edef35697b19f3736b07cf5
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-05-26 17:18:05 -0600

    Refactor test on dependencies and validations

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -2,7 +2,19 @@ require 'rails_helper'
 
 RSpec.describe Application, type: :model do
   describe 'relationships' do
-    it { should have_many :application_pets }
+    it { should have_many(:application_pets).dependent(:destroy) }
     it { should have_many(:pets).through(:application_pets) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:street_number) }
+    it { should validate_numericality_of(:street_number) }
+    it { should validate_presence_of(:street_name) }
+    it { should validate_presence_of(:street_type) }
+    it { should validate_presence_of(:city) }
+    it { should validate_presence_of(:state) }
+    it { should validate_presence_of(:zip_code) }
   end
 end
