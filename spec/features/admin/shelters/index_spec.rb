24ab289ffd7443372dffc6ea83d3f2a648f882ff
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-05-27 12:38:00 -0600

    Create test for models and features for pending method

--- spec/features/admin/shelters/index_spec.rb
+++ spec/features/admin/shelters/index_spec.rb
@@ -1,18 +1,69 @@
 require 'rails_helper'
 
 RSpec.describe 'the admin shelters index' do
-  before(:each) do
-    @shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-    @shelter_2 = Shelter.create(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
-    @shelter_3 = Shelter.create(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)
-    @shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
-    @shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
-    @shelter_3.pets.create(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true)
+  it 'lists all the shelter names in reverse alphabetical' do
+    shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+    shelter_2 = Shelter.create(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
+    shelter_3 = Shelter.create(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)
+    mr_pirate = shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
+    clawdia = shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
+    lucy_bald = shelter_3.pets.create(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true)
+    ricky_ricardo = shelter_3.pets.create(name: 'Ricky Ricardo', breed: 'aussie', age: 4, adoptable: true)
+
+    visit '/admin/shelters'
+
+    within("#list_of_shelters") do
+      expect(shelter_3.name).to appear_before(shelter_1.name)
+    end
   end
 
-  it 'lists all the shelter names in reverse alphabetical' do
-    visit "/admin/shelters"
+  it 'shows shelters with pending applications' do
+    shelter_4 = Shelter.create(name: 'Coloradog shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+    shelter_5 = Shelter.create(name: 'TSP animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
+    shelter_6 = Shelter.create(name: 'Crazy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)
+    mr_pirate = shelter_4.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
+    clawdia = shelter_4.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
+    lucy_bald = shelter_6.pets.create(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true)
+    ricky_ricardo = shelter_6.pets.create(name: 'Ricky Ricardo', breed: 'aussie', age: 4, adoptable: true)
 
-    expect(@shelter_3.name).to appear_before(@shelter_1.name)
+    @tkt_application = Application.create!(
+      first_name: 'Timothy',
+      middle_name: 'Kelly',
+      last_name: 'Tyson',
+      street_number: 8399,
+      street_name: 'Thomas',
+      street_type: 'Lane',
+      city: 'Arvada',
+      state: 'CO',
+      zip_code: '89049',
+      status: 'In Progress'
+    )
+
+    @tkt_application.pets << lucy_bald
+
+    @jms_application = Application.create!(
+      first_name: 'Jamie',
+      middle_name: 'Marie',
+      last_name: 'Sims',
+      street_number: 8399,
+      street_name: 'Thomas',
+      street_type: 'Lane',
+      city: 'Arvada',
+      state: 'CO',
+      zip_code: '89049',
+      description: 'Pet owner for many years!',
+      status: 'Pending'
+    )
+
+    @jms_application.pets << mr_pirate
+    @jms_application.pets << clawdia
+
+    visit '/admin/shelters'
+
+    within("#shelters_with_pending_apps") do
+      expect(page).to have_content(shelter_4.name)
+      expect(page).to_not have_content(shelter_5.name)
+      expect(page).to_not have_content(shelter_6.name)
+    end
   end
 end
--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -20,16 +20,80 @@ RSpec.describe Application, type: :model do
 
   before(:each) do
     @shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-    @pet_1 = @shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
-    @pet_2 = @shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
-    @pet_3 = @shelter_1.pets.create(name: 'Ann', breed: 'ragdoll', age: 3, adoptable: false)
+    @shelter_2 = Shelter.create(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
+    @shelter_3 = Shelter.create(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)
+
+    @mr_pirate = @shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: false)
+    @clawdia = @shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
+    @lucy_bald = @shelter_3.pets.create(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true)
+    @ann = @shelter_1.pets.create(name: 'Ann', breed: 'ragdoll', age: 5, adoptable: true)
   end
 
   describe 'class methods' do
+    describe '#count' do
+      it 'counts pets' do
+        @amm_application = Application.create!(
+          first_name: 'Amy',
+          middle_name: 'Mary',
+          last_name: 'Margi',
+          street_number: 8399,
+          street_name: 'Sally',
+          street_type: 'Court',
+          city: 'Arvada',
+          state: 'CO',
+          zip_code: '89429',
+          status: 'In Progress'
+        )
+        @amm_application.pets << @lucy_bald
+        @amm_application.pets << @clawdia
+        @amm_application.pets << @ann
+
+        expect(@amm_application.pet_count).to eq(3)
+      end
+    end
+
     describe '#search' do
       it 'returns partial matches' do
-        expect(Pet.search("cLaw")).to eq([@pet_2])
-        expect(Pet.search("pir")).to eq([@pet_1])
+        expect(Pet.search("cLaw")).to eq([@clawdia])
+        expect(Pet.search("pir")).to eq([@mr_pirate])
+      end
+    end
+
+    describe '#pending' do
+      it 'returns only shelters with pending applications' do
+        @tkt_application = Application.create!(
+          first_name: 'Timothy',
+          middle_name: 'Kelly',
+          last_name: 'Tyson',
+          street_number: 8399,
+          street_name: 'Thomas',
+          street_type: 'Lane',
+          city: 'Arvada',
+          state: 'CO',
+          zip_code: '89049',
+          description: 'Love pets!',
+          status: 'In Progress'
+        )
+        @tkt_application.pets << @lucy_bald
+        @tkt_application.pets << @ann
+
+        @jms_application = Application.create!(
+          first_name: 'Jamie',
+          middle_name: 'Marie',
+          last_name: 'Sims',
+          street_number: 8399,
+          street_name: 'Thomas',
+          street_type: 'Lane',
+          city: 'Arvada',
+          state: 'CO',
+          zip_code: '89049',
+          description: 'Pet owner for many years!',
+          status: 'Pending'
+        )
+        @jms_application.pets << @mr_pirate
+        @jms_application.pets << @clawdia
+
+        expect(Application.pending[0].name).to eq(@shelter_1.name)
       end
     end
   end
